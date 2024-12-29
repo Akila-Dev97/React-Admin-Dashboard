27467c4be3f861e13b5a716ae200944866801f74
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-29 22:48:31 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+
+import Sidebar from "./components/common/Sidebar";
+
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 import UsersPage from "./pages/UsersPage";
@@ -6,7 +9,6 @@ import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import SettingsPage from "./pages/SettingsPage";
-import Sidebar from "./components/common/Sidebar";
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
+
       <Sidebar />
       <Routes>
         <Route path="/" element={<OverviewPage />} />
