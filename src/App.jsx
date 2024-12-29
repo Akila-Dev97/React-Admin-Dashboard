433ae2c7a5e20c48c03f08bfae1e73d0b362021e
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-29 12:51:56 +0530

    add routing for OverviewPage and ProductsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
